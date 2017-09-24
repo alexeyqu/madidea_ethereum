9bc1335a894813178cb8e150c9599e8d96daab08
Andrey Putilin <regresscheck@gmail.com>
2017-09-24 09:14:08 +0300

    first call

---
+++ front/abi.js
@@ -0,0 +1,50 @@
+var ABI = [
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "die",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "say",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_message",
+        "type": "string"
+      }
+    ],
+    "name": "change",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "name": "_message",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
--- front/icoHack.js
+++ front/icoHack.js
@@ -1,23 +1,7 @@
-    let web3 = new Web3();
-    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-    console.log('aaa');
-    function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+var contractAddress = "0xeDFC9c2F4Cfa7495c1A95CfE1cB856F5980D5e18";
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+var contract = web3.eth.contract(ABI).at(contractAddress);
+console.log(contract.say());
