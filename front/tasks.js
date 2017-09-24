ae2988f431c1ace23a30ed2c975fe2c403909467
peterpitovsky <petr.gezhes@diginetica.com>
2017-09-24 14:31:52 +0300

    frontend fixes

--- front/tasks.js
+++ front/tasks.js
@@ -1,8 +1,9 @@
-contract.allEvents().watch(function(error, event) {
+watchEvents(function(error, event) {
     if (error) {
         console.log(error);
     }
-    if (event.event === 'ProposalCreated') {
+    console.log(event);
+    if (event.event.startsWith('getAllProposalsFor')) {
         document.getElementById('task-list').innerHTML += '<div class="description-line proposal" data-id="' + event.proposalId.c[0] + '">'
             + event.proposalName + '</div>' 
         var proposals = document.getElementByClassName('proposal');
@@ -10,14 +11,14 @@ contract.allEvents().watch(function(error, event) {
             window.location.href = 'task.html?pid=' + proposal[proposal.length - 1].dataset.id + '&from=' + getFrom();
         }
     }
-}
+});
 
 var role = getFrom();
 if (role === 'employer') {
     contract.getAllProposalsForEmployer(web3.eth.accounts[0]);
-else if (role === 'employee') {
+} else if (role === 'employee') {
     contract.getAllProposalsForEmployee(web3.eth.accounts[0]);
-else if (role === 'judge') {
+} else if (role === 'judge') {
     contract.getAllProposalsForJudge(web3.eth.accounts[0]);
 }
 
